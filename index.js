ed20d1b8805cd308aa723090076d219939c835e6
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-05-14 13:32:55 +0100

    Practice for and while loops

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names, occasion) {
+  let cardsArray = []
+  for (let i = 0; i < names.length; i++ ) {
+    cardsArray.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`)
+  }
+  return cardsArray
+}
+
+function countdown(num) {
+  while (num >= 0) {
+    console.log(num--)
+  }
+}
